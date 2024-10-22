1334e35b70ad192641f3dd1de783de43a112404a
pss0204 <ss0204.park@gmail.com>
2024-10-22 12:50:06 +0900

    레이스 명령어 추가 해서 플래그 매핑 확인용 코드

--- data_race.c
+++ data_race.c
@@ -4,11 +4,18 @@
 #define NUM_THREADS 2
 #define NUM_ITERATIONS 1000000
 
-int counter = 0;
+volatile int counter = 0;
 
 void* increment(void* arg) {
     for(int i = 0; i < NUM_ITERATIONS; i++) {
-        counter++; // 데이터 레이스 발생 지점
+        __asm__ __volatile__(
+            "lw t0, 0(%0)\n\t"    // counter 값을 t0 레지스터로 로드
+            "addi t0, t0, 1\n\t"  // t0 값에 1을 더함
+            "sw t0, 0(%0)"        // t0 값을 다시 counter에 스토어
+            :
+            : "r" (&counter)       // counter의 주소를 입력으로 사용
+            : "t0", "memory"
+        ); // 인라인 어셈블리를 사용한 직접 로드/스토어 데이터 레이스 발생 지점
     }
     return NULL;
 }
@@ -25,6 +32,14 @@ int main() {
     for(int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
+
+    // 모든 과정이 끝난 후 TEST_RACE 명령어 실행
+    __asm__ __volatile__(
+        ".word 0b00000000011100101001001011111011    # TEST_RACE x6, x5, x7"
+        :
+        :
+        : "memory"
+    );
 
     printf("최종 카운터 값: %d\n", counter);
     return 0;
