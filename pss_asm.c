1eafb39f0f49839570c9b2a13502a25c763b27ec
pss0204 <ss0204.park@gmail.com>
2024-11-01 11:13:41 +0900

    Revert "2 park 소스 레지스터 변경"
    This reverts commit 29736d3386d142dd2dc45741ea3e3c4ea9c517c9.

--- pss_asm.c
+++ pss_asm.c
@@ -4,7 +4,7 @@
 int main() {
     printf("Hello, World!\n");
     __asm__ __volatile__(
-        ".word 0b11111110110011100110111100001011   # PARK x28, x29, x30\n\t"
+        ".word 0b11111110110001011000010100001011   # PARK x10, x11, x12\n\t"
         "lw t0, 0(sp)\n\t"
         "lw t1, 4(sp)\n\t"
         "add t2, t0, t1\n\t"
@@ -15,7 +15,7 @@ int main() {
         "lw t5, 20(sp)\n\t"
         "add t6, t4, t5\n\t"
         "sw t6, 24(sp)\n\t"
-        ".word 0b11111110110011100110111100001011   # PARK x28, x29, x30\n\t"
+        ".word 0b11111110110001011000010100001011   # PARK x10, x11, x12\n\t"
         ".word 0b00000000011100101001001011111011    # TEST_RACE x6, x5, x7\n\t"
         :
         :
