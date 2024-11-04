3e2f573cb642ce1f3452e35ce3024d75cf4eb716
pss0204 <ss0204.park@gmail.com>
2024-11-04 11:26:00 +0900

    버전 9

--- pss_asm.c
+++ pss_asm.c
@@ -6,16 +6,11 @@ int main() {
     __asm__ __volatile__(
         ".word 0b11111111110011101000111100001011   # PARK x28, x29, x30\n\t"
         "lw t0, 0(sp)\n\t"
-        "lw t1, 4(sp)\n\t"
-        "add t2, t0, t1\n\t"
-        "sw t2, 8(sp)\n\t"
-        "lw t3, 12(sp)\n\t"
-        "add t4, t2, t3\n\t"
-        "sw t4, 16(sp)\n\t"
-        "lw t5, 20(sp)\n\t"
-        "add t6, t4, t5\n\t"
-        "sw t6, 24(sp)\n\t"
-         ".word 0b11111111110011101000111100001011   # PARK x28, x29, x30\n\t"
+        "sw t0, 0(sp)\n\t"
+        "lw t0, 0(sp)\n\t"
+        "add t4, t5, t6\n\t"
+        "sw t4, 0(sp)\n\t"
+        ".word 0b11111111110011101000111100001011   # PARK x28, x29, x30\n\t"
         ".word 0b00000000011100101001001011111011    # TEST_RACE x6, x5, x7\n\t"
         :
         :
