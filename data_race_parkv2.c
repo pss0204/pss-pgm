30b4fdd41375b72c081f10f5af28edadb4f44d46
pss0204 <ss0204.park@gmail.com>
2024-11-07 18:46:10 +0900

    2-4

--- data_race_parkv2.c
+++ data_race_parkv2.c
@@ -9,13 +9,14 @@ volatile int counter = 0;
 void* increment(void* arg) {
     for(int i = 0; i < NUM_ITERATIONS; i++) {
         __asm__ __volatile__(
-            "lw t0, 0(sp)\n\t"    // counter 값을 t0 레지스터로 로드
-            "addi t0, t0, 1\n\t"  // t0 값에 1을 더함
-            "sw t0, 0(sp)"        // t0 값을 다시 counter에 스토어
+            "la t1, counter\n\t"   // counter의 주소를 t1에 로드
+            "lw t0, 0(a5)\n\t"     // counter 값을 t0에 로드
+            "addi t0, t0, 1\n\t"   // t0 값 증가
+            "sw t0, 0(a5)"         // 증가된 값을 counter에 저장
             :
-            : "r" (&counter)       // counter의 주소를 입력으로 사용
-            : "t0", "memory"
-        ); // 인라인 어셈블리를 사용한 직접 로드/스토어 데이터 레이스 발생 지점
+            : 
+            : "t0", "t1", "memory"
+        );
     }
     return NULL;
 }
